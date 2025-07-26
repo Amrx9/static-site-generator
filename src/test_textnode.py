fff0ebf95be2d895485da6c0b45b83cfe1ae2006
Amr <amr.khaledt1@gmail.com>
2025-07-26 17:46:30 +0300

    feat: text_to_html

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,7 +2,6 @@ import unittest
 
 from textnode import TextNode, TextType
 
-
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.TEXT)
@@ -29,6 +28,35 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
+
+    class TestTextNodeToHTMLNode(unittest.TestCase):
+        def test_text_node_to_html_node(self):
+            node = TextNode("Hello, world!", TextType.TEXT)
+            html_node = node.text_node_to_html_node()
+            self.assertEqual(html_node.tag, None)
+            self.assertEqual(html_node.value, "Hello, world!")
+
+            node = TextNode("Bold text", TextType.BOLD)
+            html_node = node.text_node_to_html_node()
+            self.assertEqual(html_node.tag, "b")
+            self.assertEqual(html_node.value, "Bold text")
+
+            node = TextNode("Click me", TextType.LINK, "https://example.com")
+            html_node = node.text_node_to_html_node()
+            self.assertEqual(html_node.tag, "a")
+            self.assertEqual(html_node.value, "Click me")
+            self.assertEqual(html_node.props, {"href": "https://example.com"})
+
+            node = TextNode("Image alt text", TextType.IMAGE, "image.jpg")
+            html_node = node.text_node_to_html_node()
+            self.assertEqual(html_node.tag, "img")
+            self.assertEqual(html_node.value, "")
+            self.assertEqual(html_node.props, {"src": "image.jpg", "alt": "Image alt text"})
+
+
+            with self.assertRaises(ValueError):
+                node = TextNode("text", "invalid_type")
+                node.text_node_to_html_node()
 
 
 if __name__ == "__main__":
